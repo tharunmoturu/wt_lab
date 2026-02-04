988861bfad84c44f7f94e8137ac4db0d1f18f888
tharun-220533 <n220533@rguktn.ac.in>
2026-02-04 16:49:09 +0530

    Completed PHP lab tasks: variables, scope, string functions, signup validation

--- wt_task2/signup.php
+++ wt_task2/signup.php
@@ -3,29 +3,51 @@ include "db.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $fullname = mysqli_real_escape_string($conn, $_POST['fullname']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $fullname=trim($_POST['fullname']);
+    $email=trim($_POST['email']);
+    $password=trim($_POST['password']);
 
-    // Hash password (VERY IMPORTANT)
+    $fullname=htmlspecialchars($fullname);
+    $email=htmlspecialchars($email);
+
+    if(strlen($fullname)<3 || strlen($fullname)>50){
+        die("Full Name must be between 3 and 50 characters.");
+    }
+    if(strlen($password)<6){
+        die("Password must be at least 6 characters long.");
+    }
+
+    $fullname=ucwords($fullname);
+    $fullname=mysqli_real_escape_string($conn,$fullname);
+    $email=mysqli_real_escape_string($conn,$email);
+    
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+   
 
-    // Check if email already exists
-    $checkEmail = "SELECT * FROM users WHERE email='$email'";
-    $result = mysqli_query($conn, $checkEmail);
+    $check_Email="SELECT * FROM users WHERE email='$email'";
+    $result=mysqli_query($conn,$check_Email);
+
+     if (!$result) {
+        die("Database Error: Unable to check email");
+    }
 
     if (mysqli_num_rows($result) > 0) {
         echo "Email already registered!";
-    } else {
+    } 
+    else {
+
         $sql = "INSERT INTO users (fullname, email, password)
                 VALUES ('$fullname', '$email', '$hashedPassword')";
 
         if (mysqli_query($conn, $sql)) {
-            echo "Signup successful!";
-            // header("Location: signin.html");
+            echo "Signup successful! ";
+            print "Welcome to FreeLanceHub";
         } else {
-            echo "Error: " . mysqli_error($conn);
+            die("Error: Registration failed");
         }
     }
+
+
+    
 }
 ?>
